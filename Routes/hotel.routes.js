f48ac9f13d827024919b946e14b486ece7014366
ArhamX24 <arhamabdullah9045@gmail.com>
2025-01-11 01:01:10 +0530

    removed-data

--- Routes/hotel.routes.js
+++ Routes/hotel.routes.js
@@ -2,8 +2,6 @@ import express, { json } from "express";
 import {getHotels, getSingleHotel, getHotelsBySearch} from "../Controllers/hotel.controllers.js"
 import { getCategories } from "../Controllers/category.controllers.js";
 
-import hotels from "../Data/hotels.js";
-import Hotels from "../Model/hotels.model.js";
 
 const HotelRouter = express.Router();
 
@@ -11,15 +9,6 @@ HotelRouter.get("/",getHotels)
 .get("/categories", getCategories)
 .get("/:id", getSingleHotel)
 .get("/search/:city", getHotelsBySearch)
-.post("/push", async(req,res) => {
-  try {
-    let dataInDb = await Hotels.insertMany(hotels.data);
-    res.json(dataInDb)
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-)
 
 
 export default HotelRouter
